7edbeb71a904915e68b284d53ef040f3f333276a
Yavor Georgiev <fealebenpae@gmail.com>
2020-03-29 22:39:10 +0200

    set up repro

---
+++ Tests/Realm.Playground/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Realms.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "playground.realm");
+            var config = new RealmConfiguration(path);
+
+            Realm.DeleteRealm(config);
+            Realm instanceFactory() => Realm.GetInstance(config);
+
+            new WriterThread(instanceFactory);
+
+            using (var localRealm = instanceFactory())
+            {
+                var subscription = localRealm.All<TestObject>().SubscribeForNotifications((sender, changes, error) => { Console.WriteLine("got notification"); });
+                // ^ removing this line will stop all deadlocks.
+
+                while (true)
+                {
+                    localRealm.Refresh();
+                    Console.Write(".");
+                    Thread.Sleep(10);
+                }
+            }
+        }
+
+        private class TestObject : RealmObject
+        {
+            [PrimaryKey] public string ID { get; set; }
+
+            public TestObject()
+            {
+                ID = Guid.NewGuid().ToString();
+            }
+        }
+
+        private class WriterThread
+        {
+            public WriterThread(Func<Realm> instanceFactory)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    using (var localRealm = instanceFactory())
+                    {
+                        while (true)
+                        {
+                            var obj = new TestObject();
+
+                            using (var transaction = localRealm.BeginWrite())
+                            {
+                                localRealm.Add(obj);
+                                Console.WriteLine("Storing item from other thread");
+                                transaction.Commit();
+                            }
+
+                            Thread.Sleep(50);
+                        }
+                    }
+                }, TaskCreationOptions.LongRunning);
+            }
+        }
+    }
+}
--- wrappers/build.ps1
+++ wrappers/build.ps1
@@ -38,7 +38,7 @@ $vs = Get-VSSetupInstance | Select-VSSetupInstance -Latest -Require Microsoft.Vi
 $Env:path += ";$($vs.InstallationPath)\MSBuild\Current\Bin"
 
 $cmake = Join-Path $vs.InstallationPath -ChildPath "Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
-$cmakeArgs = "-DCMAKE_GENERATOR_INSTANCE=$($vs.InstallationPath)", "-DCMAKE_BUILD_TYPE=$Configuration", "-DCMAKE_SYSTEM_NAME=$Target", "-DCMAKE_INSTALL_PREFIX=$PSScriptRoot\build", "-DCMAKE_TOOLCHAIN_FILE=c:\\src\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake"
+$cmakeArgs = "-DCMAKE_GENERATOR_INSTANCE=$($vs.InstallationPath)", "-DCMAKE_BUILD_TYPE=$Configuration", "-DCMAKE_SYSTEM_NAME=$Target", "-DCMAKE_INSTALL_PREFIX=$PSScriptRoot\build"
 
 if ($Target -eq 'WindowsStore') {
     $cmakeArgs += "-DCMAKE_SYSTEM_VERSION='10.0'"
@@ -46,25 +46,11 @@ if ($Target -eq 'WindowsStore') {
     $cmakeArgs += "-DCMAKE_SYSTEM_VERSION='8.1'", "-DCMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION='8.1'"
 }
 
-function triplet([string]$target, [string]$platform) {
-    $arch = $platform.ToLower()
-    if ($arch -eq 'win32') { 
-        $arch = 'x86'
-    }
-
-    $plat = $target.ToLower()
-    if ($plat -eq 'windowsstore') {
-        $plat = 'uwp'
-    }
-
-    return "$arch-$plat-static"
-}
-
 foreach ($platform in $Platforms) {
     Remove-Item .\cmake\$Target\$Configuration-$platform -Recurse -Force -ErrorAction Ignore
     New-Item .\cmake\$Target\$Configuration-$platform -ItemType "Directory" | Out-Null
     Push-Location .\cmake\$Target\$Configuration-$platform
-    & $cmake $PSScriptRoot $cmakeArgs -DCMAKE_GENERATOR_PLATFORM="$platform" -DVCPKG_TARGET_TRIPLET="$(triplet -target $Target -platform $platform)"
+    & $cmake $PSScriptRoot $cmakeArgs -DCMAKE_GENERATOR_PLATFORM="$platform"
     & $cmake --build . --target install --config $Configuration
     Pop-Location
 }
