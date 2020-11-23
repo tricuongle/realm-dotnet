71bb7f4423d13e87b7beecabac43838519efe319
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2020-11-23 15:18:43 +0100

    Removed all code for github pages branch

--- wrappers/build-macos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-bash "$SCRIPT_DIRECTORY"/build.sh -DCMAKE_TOOLCHAIN_FILE="${SCRIPT_DIRECTORY}/realm-core/tools/cmake/macosx.toolchain.cmake" ${@:1}
